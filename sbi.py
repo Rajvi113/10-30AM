6cae9ef95420b13f1e33fda832657244cb56d5af
Rajvi113 <rajvipatel432@gmail.com>
2025-03-09 18:00:03 +0530

    added two lines

--- sbi.py
+++ sbi.py
@@ -2,3 +2,5 @@ I'd be content with a modest income.
 He's quite content living on his own.
 I'm content to sit at the back.
 They were content with second hand clothes
+Before diving into adding files in Git
+For creating a folder use command- mkdir folder-name
