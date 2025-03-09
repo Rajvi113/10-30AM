01380eb1c90a1e753b9177a2490d36aeacd22b63
EC2 Default User <ec2-user@ip-172-31-44-117.ap-south-1.compute.internal>
2025-03-09 11:34:31 +0000

    removed first line & added four lines

--- sbi.py
+++ sbi.py
@@ -1,1 +1,4 @@
-anirudh is a bad & worst boy
+I'd be content with a modest income.
+He's quite content living on his own.
+I'm content to sit at the back.
+They were content with second hand clothes
